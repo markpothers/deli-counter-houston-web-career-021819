60ef4cbdd85fc39dff0f54767e3a089b4b63a1b2
Mark Pothecary <markpothers@hotmail.com>
2019-02-10 21:43:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(line)
   if line.size > 0
       line_string = ["The line is currently:"]
         x = 0
-        while x < line_string.size
+        while x < line_string.size-1
           line_string.push(x + 1)
           line_string.push(line[x])
           x += 1
