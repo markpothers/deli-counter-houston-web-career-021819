5fc179e0648b7b7dbedf4c17dce3be2572cb888c
Mark Pothecary <markpothers@hotmail.com>
2019-02-10 21:36:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,14 @@
 # Write your code here.
 def line(line)
   if line.size > 0
-        puts "The line is currently: "
+      x = 0
+      line_string = ["The line is currently:"]
+        while x < line_string.size-1
+          line_string.push(x+1)
+          line_string.push(line[x])
+          x += 1
+        end
+      puts line_string
   else
     puts "The line is currently empty."
   end
